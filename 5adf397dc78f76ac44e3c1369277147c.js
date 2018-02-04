611104339801dee6eb878362098230a8f0fd1f11
Viktor Marchenko <viktor.marchenko@gmail.com>
2018-02-04 16:37:55 +0300

    2018-02-04T13:37

--- 5adf397dc78f76ac44e3c1369277147c.js
+++ 5adf397dc78f76ac44e3c1369277147c.js
@@ -158,7 +158,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '60107' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '50592' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
